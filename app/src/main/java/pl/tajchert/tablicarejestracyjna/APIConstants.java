fd6274b2532b848d2c18fd400e38dcd78154aa34
Michal Tajchert <mtajchert@gmail.com>
2015-05-01 15:50:32 +0200

    ApiConstants some added.

---
+++ app/src/main/java/pl/tajchert/tablicarejestracyjna/APIConstants.java
@@ -0,0 +1,17 @@
+package pl.tajchert.tablicarejestracyjna;
+
+/**
+ * Created by Michal Tajchert on 2015-05-01.
+ */
+public class APIConstants {
+    public static final String TABLICE_INFO_PLATE = "";
+    public static final String TABLICE_INFO_VOTE_ADD = "";
+    public static final String TABLICE_INFO_COMMENT_ADD = "";
+    public static final String TABLICE_INFO_COMMENT_FIELD_AGENT = "User-Agent";
+    public static final String TABLICE_INFO_COMMENT_AGENT = "";
+    public static final String TABLICE_INFO_COMMENT_FIELD_SECRET = "";
+    public static final String TABLICE_INFO_COMMENT_SECRET = "";
+    public static final String TABLICE_INFO_COMMENT_FIELD_NICK = "name";
+    public static final String TABLICE_INFO_COMMENT_FIELD_CONTENT = "text";
+    public static final String TABLICE_INFO_COMMENT_FIELD_PHOTOS = "photos[]";
+}
